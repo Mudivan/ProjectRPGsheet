0a138c546af194887c1992013820ce90679d238a
defto <defto@JAEGER>
2026-08-08 14:39:03 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿///Projeto de abstração de uma Ficha de RPG Simples.Baseada na Ficha do Sistema 3D&T
+
+//Entrada de dados - Nome do persoganem
+Console.WriteLine("Digite o nome do seu personagem");
+string name = Console.ReadLine();
+Console.WriteLine("Olá " +  name);
+
+//Entrada de dados - Raça do personagem
+Console.WriteLine("Digite a raça do seu personagem");
+string race = Console.ReadLine();
+Console.WriteLine("Olá " + race);
+
+//Entrada de dados - Classe do personagem
+Console.WriteLine("Digite a classe do seu personagem");
+string role = Console.ReadLine();
+Console.WriteLine("Olá " + role);
